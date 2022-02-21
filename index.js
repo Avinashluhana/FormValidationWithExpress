c251c0bd99ca4845b6542ce759bf193b733efae9
avinashluhana <aviluhana@gmail.com>
2022-02-21 17:36:15 +0500

    aada

--- index.js
+++ index.js
@@ -30,4 +30,7 @@ app.get("/send", (req, res) => {
 app.get("/send", (req, res) => {
   res.send("send us page");
 });
+app.get("/send", (req, res) => {
+  res.send("send us page");
+});
 app.listen();
