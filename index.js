7ad5f3b5e528e945f3fba23cd321dc1d2b481008
avinashluhana <aviluhana@gmail.com>
2022-03-05 17:12:17 +0500

    abdasf

--- index.js
+++ index.js
@@ -5,40 +5,7 @@ app.get("/", (req, res) => {
   res.send("connection established");
 });
 
-app.get("/about", (req, res) => {
-  res.send("about page");
-});
-app.get("/help", (req, res) => {
-  res.send("Help page");
-});
-app.get("/login", (req, res) => {
-  res.send("login page");
-});
-app.get("/signup", (req, res) => {
-  res.send("Sign up page");
-});
-app.get("/contact", (req, res) => {
-  res.send("Contact us page");
-});
-app.get("/send", (req, res) => {
-  res.send("send us page");
-});
-app.get("/send", (req, res) => {
-  res.send("send us page");
-});
-app.get("/send", (req, res) => {
-  res.send("send us page");
-});
-app.get("/send", (req, res) => {
-  res.send("send us page");
-});
-app.get("/send", (req, res) => {
-  res.send("send us page");
-});
-app.get("/send", (req, res) => {
-  res.send("send us page");
-});
-app.get("/send", (req, res) => {
+app.get("/send", (req, res) => {  
   res.send("send us page");
 });
 app.get("/send", (req, res) => {
@@ -51,22 +18,8 @@ console.log("outpur?");
 console.log("outpur?");
 console.log("outpur?");
 
-console.log("outpur?");
-console.log("outpur?");
 
-
-console.log("outpur?");
-console.log("testing");
-console.log("testing");
-console.log("testing");
-console.log("testing");
-
-console.log("testing");
-console.log("testing");
-console.log("testing");
-console.log("testing");
-console.log("testing");
-
+c
 
 console.log("testing");
 
