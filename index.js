86fafe3a54ae7a0d6fe871ab074aeecb57eeb439
avinashluhana <aviluhana@gmail.com>
2022-03-07 17:26:35 +0500

    aadasd

--- index.js
+++ index.js
@@ -5,25 +5,19 @@ app.get("/", (req, res) => {
   res.send("connection established");
 });
 
-app.get("/send", (req, res) => {  
-  res.send("send us page");
-});
 app.get("/send", (req, res) => {
   res.send("send us page");
 });
 app.get("/send", (req, res) => {
   res.send("send us page");
 });
+app.get("/send", (req, res) => {
+  res.send("send us page");
+});
 console.log("outpur?");
 console.log("outpur?");
 console.log("outpur?");
-
-
-c
-
 console.log("testing");
-
-
-
+console.log("testing");
 
 app.listen().addListener(3000);
